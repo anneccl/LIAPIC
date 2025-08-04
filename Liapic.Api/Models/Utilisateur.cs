6bb2c232b2685184502c382ccd5afd4e73bbd879
N'Dri Anne-Cécilia Myriam Kone <6236961@etu.cegepsth.qc.ca>
2025-08-04 12:34:11 -0400

    Création de l'entité Publication et correction de l'entité Utilisateur

--- Liapic.Api/Models/Utilisateur.cs
+++ Liapic.Api/Models/Utilisateur.cs
@@ -1,17 +1,38 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-[Table]
+/// <summary> 
+/// Représente un utilisateur inscrit sur la plateforme Liapic.
+/// Contient ses informations de profil, de connexion et de description.
+/// </summary>
+[Table("Utilisateurs")]
 public class Utilisateur
 {
     [Key]
     public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
     public string Nom { get; set; }
+
+    [Required]
+    [MaxLength(50)]
     public string Prenom { get; set; }
+
+    [Required]
+    [MaxLength(30)]
     public string NomUtilisateur { get; set; }
+
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
-    [Password]
+
+    [Required]
+    [DataType(DataType.Password)]
     public string MotDePasse { get; set; }
+
+    [MaxLength(280)]
     public string Biographie { get; set; }
-    public string PhotoProfilUrl { get; set; }
-    public DateTime DateInscription { get; set; }
+
+    [Url]
+    public string? PhotoProfilUrl { get; set; }
+
+    public DateTime DateInscription { get; set; } = DateTime.UtcNow;
 }
